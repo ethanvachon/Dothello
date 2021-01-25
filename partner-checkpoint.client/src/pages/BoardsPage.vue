f9971658f8ddded2db5075d6ebd0b87ba529c504
Daniel Madden <danielfpjobs@gmail.com>
2021-01-25 16:33:30 -0700

    literally nothing

--- partner-checkpoint.client/src/pages/BoardsPage.vue
+++ partner-checkpoint.client/src/pages/BoardsPage.vue
@@ -1,4 +1,10 @@
 <template lang="">
+  <button @click="makeBoard">
+    Make Board
+  </button>
+  <button @click="getBoards">
+    Get
+  </button>
   <div class="row justify-content-center">
     <board-component v-for="board in boards" :key="board.name" :board="board"></board-component>
   </div>
@@ -11,11 +17,22 @@ export default {
   name: 'BoardsPage',
   setup() {
     onMounted(() => {
+      boardService.postBoard({
+        name: 'this is a name'
+      })
       boardService.getBoards()
       AppState.boards = [{ name: 'Hi this is a board bitchh' }, { name: 'hi 2' }, { name: ' REEEEE' }, { name: 'goteeeem' }, { name: 'board three' }]
     })
     return {
-      boards: computed(() => AppState.boards)
+      boards: computed(() => AppState.boards),
+      makeBoard() {
+        boardService.postBoard({
+          name: 'this is a name'
+        })
+      },
+      getBoards() {
+        boardService.getBoards()
+      }
     }
   }
 }
