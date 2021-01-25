c48729aa24d2c88e9200314202c22b28334cc415
ethanvachon <ethanhvachon@gmail.com>
2021-01-25 13:23:07 -0700

    model

--- partner-checkpoint.server/server/models/ListItem.js
+++ partner-checkpoint.server/server/models/ListItem.js
@@ -4,7 +4,8 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 
 const ListItem = new Schema(
   {
-    name: { type: String, required: true }
+    name: { type: String, required: true },
+    list: { type: ObjectId, ref: 'List', required: true }
   }
 )
 
