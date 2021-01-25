3532895780de0d8f2bad3340fbd5f1a261e1e443
ethanvachon <ethanhvachon@gmail.com>
2021-01-25 13:41:22 -0700

    Models & Db

--- partner-checkpoint.server/server/models/Task.js
+++ partner-checkpoint.server/server/models/Task.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 const ObjectId = mongoose.Schema.Types.ObjectId
 
-const ListItem = new Schema(
+const Task = new Schema(
   {
     name: { type: String, required: true },
     list: { type: ObjectId, ref: 'List', required: true }
   }
 )
 
-export default ListItem
+export default Task
