0787c130b2b1cb18fa22f185cdde187717bdf67b
ethanvachon <ethanhvachon@gmail.com>
2021-03-09 19:08:19 -0700

    this is the one

--- partner-checkpoint.client/vue.config.js
+++ partner-checkpoint.client/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
   publicPath: '/',
-  outputDir: '../partner-checkpoint-server/client',
+  outputDir: '../partner-checkpoint.server/client',
   productionSourceMap: false
 }
